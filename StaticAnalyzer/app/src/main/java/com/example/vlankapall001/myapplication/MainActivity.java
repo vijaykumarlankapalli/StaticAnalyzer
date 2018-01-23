e6276f0c9c39b395fe628c4d5f5a64cf6c038e68
Vijaya Kumar Lankapalli <vijaya.kumar.lankapalli@pwc.com>
2018-01-23 17:40:08 +0530

    modified1

--- StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
+++ StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         sOkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+
+
+                   
+
                     Toast.makeText(MainActivity.this, "This is Onclick of button", Toast.LENGTH_SHORT).show();
                     String abcc= null;
                     if(abcc.contains("s")){
