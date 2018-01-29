8ed912f56650006a05debacbe01109729af796ec
Vijaya Kumar Lankapalli <vijaya.kumar.lankapalli@pwc.com>
2018-01-29 19:39:46 +0530

    commit1

--- StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
+++ StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "This is Onclick of button", Toast.LENGTH_SHORT).show();
                     String abcc= null;
                     if(abcc.contains("s")){
-
+                        //to do
 
                     }
             }
