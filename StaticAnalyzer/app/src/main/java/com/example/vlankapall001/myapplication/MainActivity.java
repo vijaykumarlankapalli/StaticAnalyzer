7471e51c3ae950e8e1136900ebf72722a7393ca1
Vijaya Kumar Lankapalli <vijaya.kumar.lankapalli@pwc.com>
2018-03-01 18:36:02 +0530

    connect1

--- StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
+++ StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Toast.makeText(MainActivity.this, "This is Onclick of button", Toast.LENGTH_SHORT).show();
+                
 
                 Toast.makeText(MainActivity.this, "This is Onclick of click", Toast.LENGTH_SHORT).show();
                     Toast.makeText(MainActivity.this, "This is Onclick of button", Toast.LENGTH_SHORT).show();
