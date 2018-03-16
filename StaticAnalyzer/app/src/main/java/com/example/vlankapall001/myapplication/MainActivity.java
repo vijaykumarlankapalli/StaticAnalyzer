0d25c5382a303adb5afa2062955b05a0740250c6
vijaykumarlankapalli <35686191+vijaykumarlankapalli@users.noreply.github.com>
2018-03-16 18:27:33 +0530

    Update MainActivity.java

--- StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
+++ StaticAnalyzer/app/src/main/java/com/example/vlankapall001/myapplication/MainActivity.java
@@ -82,9 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 if (position == 0) {
                     videoView.start();
                 }
-                 if (position == 0) {
-                    videoView.start();
-                 }
+                
                 // When video Screen change size.
                 mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
                     @Override
